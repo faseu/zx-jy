60be9a532dd7e1caefa2a1c3ff3fd05004c2cf2c
Faseu <l5713977886@gmail.com>
2025-12-27 00:24:42 +0800

    init

---
+++ src/locales/ar-SA/component.ts
@@ -0,0 +1,5 @@
+export default {
+  'component.tagSelect.expand': 'توسيع',
+  'component.tagSelect.collapse': 'انهيار',
+  'component.tagSelect.all': 'الكل',
+};
---
+++ src/locales/ar-SA/globalHeader.ts
@@ -0,0 +1,18 @@
+
+export default {
+  'component.globalHeader.search': 'البحث',
+  'component.globalHeader.search.example1': 'مثال البحث 1',
+  'component.globalHeader.search.example2': 'مثال البحث 2',
+  'component.globalHeader.search.example3': 'مثال البحث 3',
+  'component.globalHeader.help': 'المساعدة',
+  'component.globalHeader.notification': 'الإشعار',
+  'component.globalHeader.notification.empty': 'لقد قرأت جميع الإشعارات.',
+  'component.globalHeader.message': 'رسالة',
+  'component.globalHeader.message.empty': 'لقد قرأت جميع الرسائل.',
+  'component.globalHeader.event': 'حدث',
+  'component.globalHeader.event.empty': 'لقد قرأت جميع الأحداث.',
+  'component.noticeIcon.clear': 'مسح',
+  'component.noticeIcon.cleared': 'تم المسح',
+  'component.noticeIcon.empty': 'لا توجد بيانات',
+  'component.noticeIcon.view-more': 'عرض المزيد',
+};
